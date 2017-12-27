4dadfdf27fa12e6a97e1078f0bdcc2a04982506b
srsandy <sandeepranjan2007@yahoo.co.in>
2017-12-27 23:17:52 +0530

    padding corrected

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ for (let i = 0; i < numBalls; i++) {
 balls.forEach((el, i, ra) => {
   let to = {
     x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
+    y: Math.random() * 10
   };
 
   let anim = el.animate(
